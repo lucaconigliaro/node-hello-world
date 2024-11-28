718dd0e9c3077e4ea94d38b1a50c632d4a6667c5
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-28 15:08:23 +0100

    hello class 135

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+console.log("Hello Boolean!");
+
+// console.log(process.argv);
+const class135 = process.argv.slice(2);
+// console.log(class135);
+console.log(`Hello ${class135}!`);
+
+
